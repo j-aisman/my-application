a77c96848041702a153ca2f447f13b4f3fb43e51
Sergei Peredreev <speredreev@gmail.com>
2022-03-27 10:53:25 +0300

    Authorization page (without design)

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      home: Scaffold (
+          body: Container (
+            width: double.infinity,
+            child: Column (children:   [
+              SizedBox(height: 70,),
+              SizedBox(width: 110, height: 84, child: Placeholder()),
+              SizedBox(height: 20,),
+              Text ('Введите логин в виде 10 цифр номера телефона'),
+              SizedBox(height: 20,),
+              const SizedBox(width: 310,
+                child: TextField(
+                   decoration: InputDecoration(
+                     filled: true,
+                     fillColor: Color(0xFFE0E0EE),
+                     labelText: 'Телефон'
+                   ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              const SizedBox(width: 310,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFE0E0EE),
+                    labelText: 'Пароль'
+                  ),
+                ),
+              ),
+              SizedBox(height: 28,),
+              SizedBox(width: 154, height: 41, child: ElevatedButton(onPressed: () {}, child: Text("Войти"))),
+              SizedBox(height: 68,),
+              InkWell(child: Text('Регистрация'), onTap: () {} ),
+              SizedBox(height: 20,),
+              InkWell(child: Text('Забыли пароль?'), onTap: () {}),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
